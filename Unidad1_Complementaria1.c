6096d0d8bed347e583efd85c3584eb44959462f1
Daniel Hernandez Hernandez <165751237+DanielHdzHdz@users.noreply.github.com>
2025-02-24 22:32:49 -0600

    Create Unidad1_Complementaria1.c

---
+++ Unidad1_Complementaria1.c
@@ -0,0 +1,17 @@
+//Universidas Atonoma Nacional de Mexico
+//FACULTAD DE CONTADURÍA Y ADMINISTRACION
+//INFORMATICA A DISTANCIA
+//Programa creado por Daniel Hernandez Hernandez el 24/02/2025
+//Asignatura: Estructura de Datos 2331
+//Profesor: MORALES HERNANDEZ GUSTAVO ARMANDO
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola Mundo!\n");
+    return 0;
+}
+
+//Programador: Daniel Hernandez Hernandez
+//Programa para imprimir "Hola Mundo!" en la consola
+//Fecha: 24/02/2025
